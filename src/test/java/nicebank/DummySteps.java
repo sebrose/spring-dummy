d97bd8073127e6e0e5e45909d93b9325c7cf4a17
Seb Rose <seb@claysnow.co.uk>
2014-04-28 23:07:15 +0100

    Passing and failing comments

--- src/test/java/nicebank/DummySteps.java
+++ src/test/java/nicebank/DummySteps.java
@@ -7,12 +7,16 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration("classpath:cucumber.xml")
 public class DummySteps {
 
+    // Uncomment next line to get this example to work
+    //@Autowired
     private Dummy dummy;
 
+    //* START Remove the following constructor to get this example to work
     @Autowired
     public DummySteps(Dummy dummy) {
         this.dummy = dummy;
     }
+    // END Remove the preceding constructor to get this example to work */
 
     @Given("^I am a dummy$")
     public void iAmADummy() throws Throwable {
